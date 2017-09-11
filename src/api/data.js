27a1070ce3ff61357f1aa2b15e34abf2d5632222
Wayuki <adchuenhong@gmail.com>
2017-09-11 20:55:49 +1000

    Fixed dummy data

--- src/api/data.js
+++ src/api/data.js
@@ -3,8 +3,7 @@ export const results = [
     _score: 1,
     _id: '1',
     _source: {
-      title: 'Apple Inc. klsjf lsdjf ljdslkfjk lsdjflkj kdsjflkj slkdjfj sldkjfkl jsldjf kljdsklfj klsdjfkl jsdklfj' +
-      ' lksdjfdkf jksljf jsdklfj ksdjfj lksdjfkl jsdlfj lksdjfl j',
+      title: 'Apple Inc.',
       description: 'Apple Inc. is an American multinational technology company headquartered in Cupertino,' +
       ' California that designs, develops, and sells consumer electronics, computer software, and online services.',
       image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/Apple_Headquarters_in_Cupertino.jpg/1200px-Apple_Headquarters_in_Cupertino.jpg',
